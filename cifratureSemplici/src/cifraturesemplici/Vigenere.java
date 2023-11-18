e036c7fe66eb79b3c16832e0da7997efe319674a
ema05 <arrigoni.emanuele@studenti-ittfedifermi.edu.it>
2023-11-18 20:14:15 +0100

    sistemati gli ultimi dettagli progetto completo!

--- cifratureSemplici/src/cifraturesemplici/Vigenere.java
+++ cifratureSemplici/src/cifraturesemplici/Vigenere.java
@@ -71,6 +71,73 @@ public class Vigenere {
     String messdeCifrato = String.valueOf(vettoreChar); 
     return messdeCifrato;
    }
+    
+   /* public static String bruteForce(String mess, String key){ 
+     
+    char space;
+    char vettoreChar[] = mess.toCharArray();
+    char vettoreKeyChar[] = key.toCharArray();
+    int j = 0;
+    int vettoreKeyInt[] = new int [vettoreChar.length];
+    for(int i = 0; i < vettoreKeyChar.length; i++){
+        
+        vettoreKeyInt[i] = vettoreKeyChar[i];
+        
+    }
+    
+    for (int i = 0; i < vettoreChar.length; i++) {
+            vettoreChar[i] -= vettoreKeyInt[j];
+            j += 1;
+              space = vettoreChar[i];
+            if (j == vettoreKeyChar.length) {
+                j = 0;
+            }
+             if(space == '\u001d'){
+                space = ' ';
+            }
+            
+            vettoreChar[i] = space;
+        }
+    
+    String messdeCifrato = String.valueOf(vettoreChar); 
+    return messdeCifrato;
+   }*/
+    
+    
+    
+    /*
+    //BOZZA BRUTE FORCE VIGENERE: VARIE PROBLEMATICHE DA RISOLVERE
+    public static String trovaChiave(String mess){
+        char space;
+        int j = 0; 
+        char vettoreChar[] = mess.toCharArray();
+        char vettoreKeyChar[] = "1234: ".toCharArray();
+        int [] vettoreKeyInt = new int[vettoreKeyChar.length];
+        for(int i = 0; i < vettoreKeyChar.length; i++){
+        
+        vettoreKeyInt[i] = vettoreKeyChar[i];
+        
+    }
+        
+         for (int i = 0; i < vettoreChar.length; i++) {
+            vettoreChar[i] -= vettoreKeyInt[j];
+            j += 1;
+              space = vettoreChar[i];
+            if (j == vettoreKeyChar.length) {
+                j = 0;
+            }
+             if(space == '\u001d'){
+                space = ' ';
+            }
+            
+            vettoreChar[i] = space;
+        }
+        
+        String chiave = String.valueOf(vettoreChar);
+        
+        return chiave;
+        
+    }*/
 }
 
    
